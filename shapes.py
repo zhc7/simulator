49b9afcd8cdc5aa73cfecba90d57461b1910d441
张瀚宸 <bjhansen2012@outlook.com>
2022-01-16 19:52:04 +0800

    [+]: add InnerCircle

--- shapes.py
+++ shapes.py
@@ -2,10 +2,22 @@ import numpy as np
 
 
 class Circle:
-    def __init__(self, radius):
+    def __init__(self, radius, k=10**2):
         self.radius = radius
         self.shape = "Circle"
+        self.k = k
 
-    def collapse(self, e):
+    def collapse(self, place, e):
         if e.shape == "Circle":
-            return abs(self.radius + e.radius - np.linalg.norm(e.place-self.place, ord=1))
+            return abs(self.radius + e.radius - np.linalg.norm(e.place-place, ord=1)) * self.k
+
+
+class InnerCircle:
+    def __init__(self, radius, k=10**4):
+        self.radius = radius
+        self.shape = "InnerCircle"
+        self.k = k
+
+    def collapse(self, place, e):
+        if e.shape == "Circle":
+            return abs(self.radius - e.radius - np.linalg.norm(e.place-place, ord=1)) * self.k
