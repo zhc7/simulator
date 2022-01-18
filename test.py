723522c716f34353fb3b1300d03deb89739bed6e
张瀚宸 <bjhansen2012@outlook.com>
2022-01-18 09:40:22 +0800

    [^]: update demo

--- test.py
+++ test.py
@@ -6,21 +6,28 @@ from simulator import World
 from Entity import Entity
 
 
-Box = Entity(InnerCircle(10), 1, np.array([0, 0], dtype="float32"), 0, 0, fixed=True)
-c1 = Entity(Circle(1), 2, np.array([0, 0], dtype="float32"), np.array([3, 4], dtype="float32"), 0)
-c2 = Entity(Circle(1), 2, np.array([3, 5], dtype="float32"), np.array([5, -4], dtype="float32"), 0)
-c3 = Entity(Circle(1), 2, np.array([-3, -4], dtype="float32"), np.array([6, 0], dtype="float32"), 0)
-c4 = Entity(Circle(1), 2, np.array([-10, 10], dtype="float32"), np.array([0, 0], dtype="float32"), 0)
+constants = {"g": np.array([0, -9.8]),
+             "epsilon0": 8.854 * 10 ** (-12),
+             "pi": 3.14159
+             }
 
 
-world = World([Box, c4, c1, c2, c3], {"g": np.array([0, 0])})
+Box = Entity(InnerCircle(20), 1, 0, np.array([0, 0], dtype="float32"), 0, 0, fixed=True)
+c1 = Entity(Circle(1), 2, 0.0001, np.array([0, 0], dtype="float32"), np.array([3, 4], dtype="float32"), 0)
+c2 = Entity(Circle(1), 2, -0.0001, np.array([3, 5], dtype="float32"), np.array([5, -4], dtype="float32"), 0)
+c3 = Entity(Circle(1), 2, -0.0001, np.array([-3, -4], dtype="float32"), np.array([6, 0], dtype="float32"), 0)
+c4 = Entity(Circle(1), 2, 0, np.array([-10, 10], dtype="float32"), np.array([0, 0], dtype="float32"), 0)
+
+
+world = World([Box, c1, c2, c3], constants, tic=0.0001)
 
 if __name__ == '__main__':
-    world.total_time = 30
+    world.total_time = 10
     t1 = time.time()
     result = world.loop()
     t2 = time.time()
     print(t2 - t1)
     print(len(pickle.dumps(result)), sep="\n")
     p = Player(result[0])
+    # input()
     p.play(result)
